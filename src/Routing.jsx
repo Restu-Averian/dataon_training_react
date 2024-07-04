2dc8160f0b4769e1aafe87e3c62d77e5fd799b99
Restu-Averian <restuaverianputra123@gmail.com>
2024-07-04 09:48:41 +0700

    feat: add routing

---
+++ src/Routing.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+
+const Home = lazy(() => import("./pages/Home"));
+
+const Routing = () => {
+  return (
+    <Suspense fallback={<>Loading...</>}>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+    </Suspense>
+  );
+};
+export default Routing;
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Home from "./pages/Home.jsx";
+import { BrowserRouter } from "react-router-dom";
+import Routing from "./Routing";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routing />
+    </BrowserRouter>
   </React.StrictMode>
 );
