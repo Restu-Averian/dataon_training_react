cff6663fba72238ded4629ed735b52fc08fa1863
Restu-Averian <restuaverianputra123@gmail.com>
2025-01-09 14:00:14 +0700

    feat: add notfound

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
 const Home = lazy(() => import("./pages/Home"));
+const NF = lazy(() => import("./pages/NF"));
 
 const Routing = () => {
   return (
     <Suspense fallback={<>Loading....</>}>
       <Routes>
         <Route path="/" Component={Home} />
+        <Route path="*" Component={NF} />
       </Routes>
     </Suspense>
   );
